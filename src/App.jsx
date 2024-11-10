528787200ce6d59c65e4e5c8f0b4d899550a2b60
PrimoIeyasu <primoieyasu1@gmail.com>
2024-11-11 00:07:34 +0800

    123123

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AboutMe from '@/components/AboutMe'; // Adjusted import for correct path
 export default function App() {
   return (
     <div>
+      <Header />
       {/* Sections */}
       <section
         id="home"
