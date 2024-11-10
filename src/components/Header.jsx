1323c26e64b64a26732f571406bcab4ae0475bd1
PrimoIeyasu <primoieyasu1@gmail.com>
2024-11-10 22:08:17 +0800

    image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "./ui/button";
-import NavBar from "./NavBar";
-import MobileNav from "./MobileNav";
-import { motion } from "framer-motion"; // Import motion from framer-motion
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './ui/button';
+import NavBar from './NavBar';
+import MobileNav from './MobileNav';
+import { motion } from 'framer-motion'; // Import motion from framer-motion
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -13,9 +13,9 @@ const Header = () => {
   };
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -23,8 +23,8 @@ const Header = () => {
     <motion.header
       className={`fixed top-0 left-0 right-0 transition-all duration-700 text-white z-50 ${
         isScrolled
-          ? "bg-black bg-opacity-70 shadow-lg py-5"
-          : "bg-transparent py-8"
+          ? 'bg-black bg-opacity-70 shadow-lg py-5'
+          : 'bg-transparent py-8'
       }`}
       initial={{ opacity: 0 }} // Set initial opacity to 0
       animate={{ opacity: 1 }} // Animate to full opacity
@@ -33,10 +33,10 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center transition-all duration-700 max-h-14">
         <Link to="/">
           <img
-            src="/assets/BlueLogohd.png"
+            src="https://primoieyasu.github.io/vergara-portfolio/assets/BlueLogohd.png"
             alt="Logo"
             className={`transition-all duration-300 ${
-              isScrolled ? "h-32 w-auto" : "h-40 w-auto"
+              isScrolled ? 'h-32 w-auto' : 'h-40 w-auto'
             }`}
           />
         </Link>
@@ -46,9 +46,9 @@ const Header = () => {
             <Button
               className="rounded-full bg-[#66FCF1] px-4 py-2 hover:bg-[#50c6be] text-black hover:text-white"
               onClick={() => {
-                const contactSection = document.getElementById("contact");
+                const contactSection = document.getElementById('contact');
                 if (contactSection) {
-                  contactSection.scrollIntoView({ behavior: "smooth" });
+                  contactSection.scrollIntoView({ behavior: 'smooth' });
                 }
               }}
             >
