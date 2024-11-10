ea6c382991df18ed9da6018f96f43d41e6309bb9
PrimoIeyasu <primoieyasu1@gmail.com>
2024-11-10 22:13:30 +0800

    image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center transition-all duration-700 max-h-14">
         <Link to="/">
           <img
-            src="https://primoieyasu.github.io/vergara-portfolio/assets/BlueLogohd.png"
+            src="/assets/BlueLogohd.png"
             alt="Logo"
             className={`transition-all duration-300 ${
               isScrolled ? 'h-32 w-auto' : 'h-40 w-auto'
