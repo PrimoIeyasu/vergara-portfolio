f6ffd0c900016740e1b748bad607cd3674a84011
Primoieyasu <primoieyasu1@gmail.com>
2025-06-14 18:28:32 +0800

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vergara-portfolio',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Adjust if your src folder is located elsewhere
